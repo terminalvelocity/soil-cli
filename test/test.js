7e5947a72e22ff5962a93f6ae3a31d0eab802508
John-Henry Liberty <johnhenry.liberty@gmail.com>
2015-06-02 02:56:54 -0500

    adds eslint, mocha, chai, in preparation for tests. Also adds optional colors string instance methods

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var Seeds = require('../index');
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+
+    it('should return the proper index when the value is present', function(){
+      assert.equal(0, [1,2,3].indexOf(1));
+    });
+  });
+
+});
+
+describe('Seeds', function() {
+  it('should return a function', function() {
+    assert.equal(typeof Seeds, typeof Function);
+  });
+});
